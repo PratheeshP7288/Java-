1b9e86a4815ac5029a21d286654a3ba86fa1d79f
Pratheesh P <161422897+PratheeshP7288@users.noreply.github.com>
2024-03-04 13:01:47 +0530

    Create index.js

---
+++ Basics/index.js
@@ -0,0 +1,77 @@
+/*var a=10
+var b=20
+function add()
+{
+    console.log(a+b)
+}
+add()
+*/
+
+ /*var factor="Kamal"
+ var fplayer="Dhoni"
+ var fmovie="Anbe Sivam"
+ function favourite()
+ {
+ console.log("Favorite Actor:",factor)
+ console.log("Favorite Playere:",fplayer)
+ console.log("Favorite Movie:",fmovie)
+ }
+ favourite()
+ */
+
+ /*
+ function area(length,breadth)
+ {
+console.log("The Area Is:"+length*breadth)
+ }
+ area(2,3)
+ */
+
+/* function add(a,b)
+ {
+    return(a+b)
+ }
+ var total=add(2,8)
+console.log(total)
+*/
+/*
+var season="fall"
+if(season=="spring")
+{
+    console.log("Enjoy the blooming flowers.")
+}
+else if(season=="summer")
+{
+    console.log("Have fun inthe sun")
+}
+else if(season=="autumn"||"fall")
+{
+    console.log("Admire the colourful leaves")
+}
+else if(season="winter")
+{
+    console.log("Bundle up and stay warm")
+}
+*/
+/*var score="50"
+if(score<=50)
+{
+    console.log("You need to improve")
+}
+else  if(score>50 && score<70)
+{
+    console.log("Good Job!")
+}
+else{
+    console.log("Excellent Performance")
+}
+*/
+/*for(count=1;count<=10;count=count+1)
+{
+    console.log(count)
+}
+*/
+for(count=1;count<=10;count=count+1)
+{
+    console.log(count+"*2=")
+}
