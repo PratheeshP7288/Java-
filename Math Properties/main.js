8ec7d9690cb30ac680c3b34c57227cfb691b674c
Pratheesh P <161422897+PratheeshP7288@users.noreply.github.com>
2024-03-13 12:48:56 +0530

    Add files via upload

---
+++ Math Properties/main.js
@@ -0,0 +1,17 @@
+//Math Properties
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI))
+console.log(Math.round(Math.PI+0.57))
+console.log(Math.ceil(Math.PI+0.57))
+console.log(Math.floor(Math.PI+0.57))
+//Math.Pow
+console.log(Math.pow(2,4))
+//Min/Max function
+console.log(Math.min(55,44,65,36,99,))
+console.log(Math.max(55,44,65,36,99,))
+console.log(Math.floor((Math.random()*100+1)))
+
+
+var a="Leena"
+console.log(a)
+console.log(a.charAt((Math.floor(Math.random()*a.length))))
