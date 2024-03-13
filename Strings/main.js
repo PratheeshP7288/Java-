2ef82583982d2f20972dd0a32f2aa0dd59472a2b
Pratheesh P <161422897+PratheeshP7288@users.noreply.github.com>
2024-03-13 10:36:38 +0530

    Add files via upload

---
+++ Strings/main.js
@@ -0,0 +1,28 @@
+//Strings
+let myvariable="Hello World! Hello"
+console.log(myvariable)
+console.log(typeof(myvariable))
+console.log(myvariable.length)
+//String Methods//
+console.log(myvariable.charAt(0))
+console.log(myvariable.charAt(11))
+//InsexOf
+console.log(myvariable.indexOf("e"))
+console.log(myvariable.indexOf("ld"))
+console.log(myvariable.lastIndexOf("Hello")) 
+//slice
+console.log(myvariable.slice(0,7))
+console.log(myvariable.slice(2,7))
+//UpperCase
+console.log(myvariable.toUpperCase())
+//LowerCase
+console.log(myvariable.toLowerCase())
+//Includes
+console.log(myvariable.includes("Wor"))
+console.log(myvariable.includes("llo"))
+console.log(myvariable.includes("lelo"))
+//Split
+console.log(myvariable.split(" "))
+console.log(myvariable.split("o"))
+console.log(myvariable.split(""))
+
